b48fee89e157ab89a10b6e4e9e11c7261e857219
rcbrett <rcbrett@gmail.com>
2017-01-22 11:08:03 -0800

    Added smoothing of the temperature data

--- Xbee_Environment_Sensors.ino
+++ Xbee_Environment_Sensors.ino
@@ -26,14 +26,12 @@ SoftwareSerial XBee(2, 3); //RX, TX
 
 const int numReadings = 10; // size of the array
 
-int readings[numReadings];  // the readings from analog input
+float readings[numReadings];  // the readings from analog input
 int readIndex = 0;          // the index of the current reading
-int total = 0;              // the running total
-int average = 0;            // the average
-
- // read the input on analog pin 0:
-  int sensorValueT = analogRead(A3);
-  int sensorValueH = analogRead(A1);
+float total = 0;              // the running total
+float average = 0;            // the average
+const int supplyVoltage = 5;
+ 
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -41,19 +39,49 @@ void setup() {
   Serial.begin(9600);
   XBee.begin(9600);
 
-  
+  for (int thisReading = 0; thisReading < numReadings; thisReading++){
+    readings[thisReading] = 0;
+  }
 }
-  const int supplyVoltage = 5;
+  
+
+
+
 // the loop routine runs over and over again forever:
 void loop() {
   float degreesC, sensorRH, trueRH, TdewPoint, CloudBase;
 
  
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  // read the input on analog pin 0:
+  int sensorValueT = analogRead(A3);
+  int sensorValueH = analogRead(A1);
   float voltageT = sensorValueT * (supplyVoltage / 1024.0);
   float voltageH = sensorValueH * (supplyVoltage / 1024.0);
+  
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = (((analogRead(A3)* (supplyVoltage / 1024.0)) - 0.5) * 100.0) - 5;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings; //smoothed T data
+  
   // Calculate temperature
+
    degreesC = ((voltageT - 0.5) * 100.0)-5;
+   
+  
   // Calculate Humidity
    sensorRH = 161.0 * voltageH / supplyVoltage - 25.8;
    trueRH = sensorRH / (1.0546 - (0.0026 * degreesC)); //temperature adjustment
@@ -63,6 +91,8 @@ void loop() {
   XBee.print(voltageT);
   XBee.print(" ");
   XBee.print(degreesC);
+  XBee.print(" ");
+  XBee.print(average);
   XBee.print(" ");
   XBee.print(voltageH);
   XBee.print(" ");
